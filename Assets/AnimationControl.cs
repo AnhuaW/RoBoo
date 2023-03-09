64e734c1c27afacc7252579d27efa93791940772
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-03-09 16:52:55 -0500

    added running animation, working on input to animator script

---
+++ Assets/AnimationControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator player_anim;
+    PlayerMovementControl player_movement;
+    float inputX;
+    float inputY;
+    void Start()
+    {
+        player_anim = GetComponent<Animator>();
+        player_movement = GetComponent<PlayerMovementControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = player_movement.playerDirection.x;
+        player_anim.SetFloat("inputX", inputX);
+        inputY = player_movement.playerDirection.y;
+        player_anim.SetFloat("inputY", inputY);
+        if(inputX == 0 && inputY == 0)
+        {
+            player_anim.SetBool("idle", true);
+        }
+        else
+        {
+            player_anim.SetBool("idle", false);
+        }
+    }
+
+}
