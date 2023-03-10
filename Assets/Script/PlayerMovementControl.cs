408353e77b4345cbc95ecc4a3fcb6dd4f09f72fb
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-03-10 15:24:01 -0500

    added groundcheck for jumping scripts

--- Assets/Script/PlayerMovementControl.cs
+++ Assets/Script/PlayerMovementControl.cs
@@ -8,11 +8,16 @@ public class PlayerMovementControl : MonoBehaviour
     public Vector2 playerDirection;
     public bool movement_frozen = false;
     public float jumpHeight = 2.5f;
+    [Space]
+    public float collisionRadius = 0.2f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
 
     Rigidbody2D rb;
     private Vector2 current_input;
     private float horizontal_input;
     private float vertical_input;
+    private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +28,7 @@ public class PlayerMovementControl : MonoBehaviour
     void Update()
     {
         current_input = Vector2.zero;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, collisionRadius, groundLayer);
 
         if (!movement_frozen)
         {
@@ -38,13 +44,13 @@ public class PlayerMovementControl : MonoBehaviour
             rb.freezeRotation = false;
         }
 
-       
+        //JumpScale();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = current_input * movement_speed;
-        if (Input.GetKeyDown(KeyCode.Space) && !movement_frozen)
+        if (Input.GetKeyDown(KeyCode.Space) && !movement_frozen && isGrounded)
         {
             if (GetComponent<GravityControl>().enable_gravity)
             {
@@ -82,8 +88,22 @@ public class PlayerMovementControl : MonoBehaviour
     void Jump()
     {
         float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * rb.gravityScale));
+        rb.velocity = new Vector2(horizontal_input, jumpForce);
+       
         Debug.Log(jumpForce);
-        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        //rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         Debug.Log("jump");
+    }
+
+    void JumpScale()
+    {
+        if (rb.velocity.y < 0 && !isGrounded)
+        {
+            rb.gravityScale = 20f;
+        }
+        else
+        {
+            rb.gravityScale = 10f;
+        }
     }
 }
