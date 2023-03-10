3e40bf4d6561e25644f27e231a38de110f5b21cf
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-03-10 00:03:55 -0500

    fixed incorrect collider size due to idle animation

--- Assets/Script/PlayerMovementControl.cs
+++ Assets/Script/PlayerMovementControl.cs
@@ -7,10 +7,9 @@ public class PlayerMovementControl : MonoBehaviour
     public float movement_speed = 4;
     public Vector2 playerDirection;
     public bool movement_frozen = false;
-    public float jumpHeight = 1f;
+    public float jumpHeight = 2.5f;
 
     Rigidbody2D rb;
-
     private Vector2 current_input;
     private float horizontal_input;
     private float vertical_input;
@@ -30,10 +29,6 @@ public class PlayerMovementControl : MonoBehaviour
             current_input = GetInput();
         }
 
-        rb.velocity = current_input * movement_speed;
-
-
-
         if (GetComponent<GravityControl>().enable_gravity)
         {
             rb.freezeRotation = true;
@@ -42,10 +37,13 @@ public class PlayerMovementControl : MonoBehaviour
         {
             rb.freezeRotation = false;
         }
+
+       
     }
 
     private void FixedUpdate()
     {
+        rb.velocity = current_input * movement_speed;
         if (Input.GetKeyDown(KeyCode.Space) && !movement_frozen)
         {
             if (GetComponent<GravityControl>().enable_gravity)
@@ -62,7 +60,6 @@ public class PlayerMovementControl : MonoBehaviour
         if (GetComponent<GravityControl>().enable_gravity)
         {
             vertical_input = 0;
-            movement_speed = 4;
         }
 
         else if (!GetComponent<GravityControl>().enable_gravity)
@@ -85,6 +82,7 @@ public class PlayerMovementControl : MonoBehaviour
     void Jump()
     {
         float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * rb.gravityScale));
+        Debug.Log(jumpForce);
         rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         Debug.Log("jump");
     }
