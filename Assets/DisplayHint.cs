539aaff498e4be1a590d10ee5f1667cd9cee66e1
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-03-18 01:17:46 -0400

    finished ammo bar UI,working on hint board

--- Assets/DisplayHint.cs
+++ Assets/DisplayHint.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DisplayHint : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Canvas hint;
+    GameObject player;
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            hint.enabled = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            hint.enabled = false;
+        }
     }
 }
