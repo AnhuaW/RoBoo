067c51d61bc1de92d76638909eb8283c2566d589
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-03-18 14:17:33 -0400

    reimplemented guide1 and fixed some layering issues

--- Assets/Script/ArrowKeyMovement.cs
+++ Assets/Script/ArrowKeyMovement.cs
@@ -49,7 +49,7 @@ public class ArrowKeyMovement : MonoBehaviour
             Vector3 current_input = GetInput();
             float speed = (GetComponent<PlayerFloat>().is_floating ? float_movement_speed : ground_movement_speed);
             transform.position += current_input * speed * Time.fixedDeltaTime;
-            if (Input.GetKeyDown(KeyCode.Space) && !GetComponent<PlayerFloat>().is_floating && isGrounded)
+            if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && !GetComponent<PlayerFloat>().is_floating && isGrounded)
             {
                 Jump();
             }
