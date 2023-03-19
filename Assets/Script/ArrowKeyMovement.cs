bf92d06a5e3ddca4dc9526fed4bf574c1b08865a
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-03-19 19:16:27 -0400

    air jump not yet fixed

--- Assets/Script/ArrowKeyMovement.cs
+++ Assets/Script/ArrowKeyMovement.cs
@@ -32,13 +32,11 @@ public class ArrowKeyMovement : MonoBehaviour
         Debug.DrawRay(right_edge, Vector2.down * 0.1f, Color.red);
         RaycastHit2D hit_left = Physics2D.Raycast(left_edge, Vector2.down, 0.1f);
         RaycastHit2D hit_right = Physics2D.Raycast(right_edge, Vector2.down, 0.1f);
-
         if ((hit_left && !hit_left.collider.isTrigger) || (hit_right && !hit_right.collider.isTrigger))
         {
 
             isGrounded = true;
         }
-
         else
         {
             isGrounded = false;
@@ -50,8 +48,12 @@ public class ArrowKeyMovement : MonoBehaviour
         {
             Vector3 current_input = GetInput();
             float speed = (GetComponent<PlayerFloat>().is_floating ? float_movement_speed : ground_movement_speed);
+            /*if(rb.velocity != Vector2.zero)
+            {
+                rb.velocity = Vector2.zero;
+            }*/
             transform.position += current_input * speed * Time.fixedDeltaTime;
-            if ((Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow)) && !GetComponent<PlayerFloat>().is_floating && isGrounded)
+            if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && !GetComponent<PlayerFloat>().is_floating && isGrounded)
             {
                 Jump();
             }
