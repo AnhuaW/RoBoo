1ffd4484aabb4a955b697bc207090bf4294d3128
Elena Bai <72651795+Elena-Baixy@users.noreply.github.com>
2023-03-19 18:55:50 -0400

    menu and background

--- Assets/Scripts_p3/DisplayPrompt.cs
+++ Assets/Scripts_p3/DisplayPrompt.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DisplayPrompt : MonoBehaviour
 {
     // Start is called before the first frame update
     public Text prompt;
-    string ammoPrompt = "Congratulations on collecting the bubble ammo! Now select with cursor and make the target float.";
+    string sceneName;
+    GameObject load;
+    string ammoPrompt = "Dear Diary," +
+        "\n\n" +"  It has been two days since I lost contact with my teammates. Last time, the signal they sent said they"
+        +" COULD NOT swim in that Lost Sea Ruins." +"\n" +"  Their oxygen is about to running out. I have to go find them" +"\n"
+        +"  Wish me good look!";
     void Start()
     {
-
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+        load = GameObject.Find("levelLoader");
+        StartCoroutine(typeWritter(ammoPrompt,sceneName));
     }
 
     // Update is called once per frame
@@ -19,16 +28,7 @@ public class DisplayPrompt : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("firstAmmo"))
-        {
-            Debug.Log("displaying prompt");
-            StartCoroutine(typeWritter(ammoPrompt));
-        }
-    }
-
-    IEnumerator typeWritter(string input)
+    IEnumerator typeWritter(string input,string sceneName)
     {
         string[] letters = input.Split(' ');
         prompt.text = letters[0];
@@ -37,7 +37,13 @@ public class DisplayPrompt : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
             prompt.text += " " + letters[i];
         }
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(10f);
         prompt.text = "";
+        if(sceneName == "Background_story")
+        {
+            Debug.Log(prompt);
+            SceneManager.LoadScene("p3_guide_1");
+            //StartCoroutine(load.GetComponent<levelLoder>().LoadLevel(1));
+        }
     }
 }
