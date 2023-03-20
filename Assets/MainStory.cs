1243928686530a1cc8d2ad8528296e18031d0e3a
jijie0918 <jijie@umich.edu>
2023-03-20 06:30:08 -0400

    Update story scene and new game button

--- Assets/MainStory.cs
+++ Assets/MainStory.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MainStory : MonoBehaviour
 {
+    GameObject load;
+    public int index;
     // Start is called before the first frame update
     void Start()
     {
-        
+        load = GameObject.Find("levelLoader");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadSceneOnClick()
+    {
+        StartCoroutine(load.GetComponent<levelLoder>().LoadLevel(index));
     }
 }
