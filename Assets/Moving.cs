feb9466e6e4a5ab3334b24b2358f9fb0e5a40935
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-03-25 17:37:59 -0400

    working on moving platform script

--- Assets/Moving.cs
+++ Assets/Moving.cs
@@ -7,43 +7,65 @@ public class Moving : MonoBehaviour
     // Start is called before the first frame update
     public float horizontal_direction = 1;
     public float vertical_direction = 0;
-    public float speed = 4f;
-    public float movaing_range = 5f;
+    public float move_speed = 4f;
+    public float move_range = 5f;
     public bool is_moving;
     float orig_posX;
     float orig_posY;
-    float curr_posX;
-    float curr_posY;
+    float target_posX;
+    float target_posY;
     Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        //get initial pos
         orig_posX = transform.position.x;
         orig_posY = transform.position.y;
+        //set target to orig
+        target_posX = orig_posX;
+        target_posY = orig_posY;
+        //calculate target based on desired moving direction
+        CalcTarget();
+        Debug.Log(target_posX);    
     }
 
     // Update is called once per frame
     void Update()
     {
-        curr_posX = transform.position.x;
-        curr_posY = transform.position.y;
+        if (horizontal_direction != 0)
+        {
+            //Move the platform horizontally
+            if(Mathf.Abs(target_posX - transform.position.x) <= move_range)
+            {
+                Debug.Log(transform.position.x);
+                rb.velocity = new Vector2(horizontal_direction * move_speed, 0);
+            }
+
+        }
+
+        if (vertical_direction != 0)
+        {
+            //Move the platform vertically
+            if(Mathf.Abs(target_posY - transform.position.y) <= move_range)
+            {
+                rb.velocity = new Vector2(0, vertical_direction * move_speed);
+            }
+        }
     }
 
-    IEnumerator MovePlatformLR()
+    void CalcTarget()
     {
-        is_moving = true;
-        yield return null;
+        if (horizontal_direction != 0)
+        {
+            target_posX = orig_posX + horizontal_direction * move_range;
+            target_posY = transform.position.y;
+        }
 
+        if (vertical_direction != 0)
+        {
+            target_posY = orig_posY + vertical_direction * move_range;
+            target_posX = transform.position.x;
+        }
     }
-
-    IEnumerator MovePlatformUD()
-    {
-        is_moving = true;
-        yield return null;
-    }
-
-
-    
-
 
 }
