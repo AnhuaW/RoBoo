e1eb7b93e227dae24d1f08f0e74959500f84de35
kawasemii <bn_kiwami@outlook.com>
2023-03-27 05:30:18 -0400

    added cheat code (press 1 to gain access to all levels)

---
+++ Assets/Scripts_p3/Cheat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GameObject completed_levels = GameObject.Find("CompletedLevels");
+            if (completed_levels != null)
+            {
+                CompletedLevels c = completed_levels.GetComponent<CompletedLevels>();
+                for (int i = 0; i < c.level_completed.Count; ++i)
+                {
+                    c.level_completed[i] = true;
+                }
+            }
+        }
+    }
+}
