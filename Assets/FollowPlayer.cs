489c636b1c157da5d9e54f60b3e9768804d9f415
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-03-27 20:15:22 -0400

    script for object to track player

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 5.0f;
+    public float stopDistance = 2.0f;
+
+    void FixedUpdate()
+    {
+        // Calculate the direction and distance to the player
+        Vector2 direction = player.position - transform.position;
+        float distance = direction.magnitude;
+
+        // Move towards the player if they are far away
+        if (distance > stopDistance)
+        {
+            direction.Normalize();
+            transform.position += (Vector3)direction * moveSpeed * Time.deltaTime;
+        }
+    }
+}
