d6ec182eda295b5d0626d1225089cb638c9f672b
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-03-31 19:39:52 -0400

    new tile palette

--- Assets/hintControl.cs
+++ Assets/hintControl.cs
@@ -14,25 +14,5 @@ public class hintControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        curr_view_pos = Camera.main.WorldToViewportPoint(transform.position);
-        if (curr_view_pos.x >= 0.1f && curr_view_pos.x <= 0.9f )
-        {
-            sprites = GetComponentsInChildren<SpriteRenderer>();
-            foreach (SpriteRenderer sprite in sprites)
-            {
-                sprite.enabled = true;
-            }
-        }
-
-        else
-        {
-            sprites = GetComponentsInChildren<SpriteRenderer>();
-            foreach (SpriteRenderer sprite in sprites)
-            {
-                sprite.enabled = false;
-            }
-        }
-    }
+    
 }
