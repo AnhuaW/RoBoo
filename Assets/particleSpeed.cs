be9f4968f7106e42002669d541890f1ce66d68e8
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-04-02 16:21:54 -0400

    added particle speed script

---
+++ Assets/particleSpeed.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleSpeed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    ParticleSystem waterParticle;
+    [SerializeField] float particle_speed;
+    void Start()
+    {
+        waterParticle = GetComponent<ParticleSystem>();
+        particle_speed = GetComponent<WaterFlow>().added_speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var main = waterParticle.main;
+        main.simulationSpeed = particle_speed;
+    }
+}
