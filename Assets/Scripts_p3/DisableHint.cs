9602b7dcc2edaf10723ee33b19854e2433f6dc6a
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-04-07 01:41:32 -0400

    completed pop guidance

--- Assets/Scripts_p3/DisableHint.cs
+++ Assets/Scripts_p3/DisableHint.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class DisableHint : MonoBehaviour
 {
     public GameObject hint;
+    public int target_clicks = 1;
+    [SerializeField]
+    private int curr_clikcs = 0;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0) && GetComponent<Floatable>().is_floating)
         {
-            GameObject.Destroy(hint);
+            curr_clikcs += 1;
+        }
+
+        if(curr_clikcs == target_clicks)
+        {
+            TurnOffHint();
+        }
+    }
+
+
+    void TurnOffHint()
+    {
+         GameObject.Destroy(hint);
             // disable the blinking script of brick
             if (GetComponent<Blinking>() != null)
             {
@@ -25,9 +36,7 @@ public class DisableHint : MonoBehaviour
                 //Debug.Log(GetComponent<SpriteRenderer>().color);
             }
             this.enabled = false;
-        }
     }
-
 
 
     IEnumerator SpriteTurnWhite(float duration)
@@ -40,4 +49,5 @@ public class DisableHint : MonoBehaviour
             yield return null;
         }
     }
+
 }
