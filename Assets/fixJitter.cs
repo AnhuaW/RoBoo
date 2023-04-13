0ee570cd51f13f1a40d84e2c93b061c72144215b
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2023-04-13 15:04:14 -0400

    updated pop guidance

--- Assets/fixJitter.cs
+++ Assets/fixJitter.cs
@@ -7,12 +7,7 @@ public class fixJitter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        QualitySettings.vSyncCount = 1;
         
     }
 }
